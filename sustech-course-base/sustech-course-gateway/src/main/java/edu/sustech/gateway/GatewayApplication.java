45eb6399b853b9290ec806450a5d1f3ffe64a155
Yuxian Wu <12212614@mail.sustech.edu.cn>
2024-11-07 05:31:56 +0800

    nacos服务注册

---
+++ sustech-course-base/sustech-course-gateway/src/main/java/edu/sustech/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package edu.sustech.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 网关启动类
+ * </p>
+ *
+ * @author Yuxian Wu
+ * @version 1.0
+ * @since 2024-11-07 4:43
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
